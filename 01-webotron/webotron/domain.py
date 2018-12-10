e01f56d15f397acc91fd56077094fe37becb0f37
Lee Wiegman <lwiegma1@progressive.com>
2018-12-10 10:08:13 -0500

    Added domain manager

---
+++ 01-webotron/webotron/domain.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import uuid
+"""Classes for Route 53 domains."""
+
+
+class DomainManager:
+    """Manage a Route 53 Domain"""
+    def __init__(self, session):
+        self.session = session
+        self.client = self.session.client('route53')
+
+    def find_hosted_zone(self, domain_name):
+        paginator = self.client.get_paginator('list_hosted_zones')
+        for page in paginator.paginate():
+            for zone in page['HostedZones']:
+                if domain_name.endswith(zone['Name'][:-1]):
+                    return zone
+
+    def create_hosted_zone(self, domain_name):
+        zone_name = '.'.join(domain_name.split('.')[-2:]) + '.'
+        return self.client.create_hosted_zone(
+            Name=zone_name,
+            CallerReference=str(uuid.uuid4())
+        )
+
+    def create_s3_domain_record(self, zone, domain_name, endpoint):
+        return self.client.change_resource_record_sets(
+            HostedZoneId=zone['Id'],
+            ChangeBatch={
+                'Comment': 'Created by webotron',
+                'Changes': [{
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': domain_name,
+                        'Type': 'A',
+                        'AliasTarget': {
+                            'HostedZoneId': endpoint.zone,
+                            'DNSName': endpoint.host,
+                            'EvaluateTargetHealth': False
+                        }
+                    }
+                }
+                ]
+            }
+        )
